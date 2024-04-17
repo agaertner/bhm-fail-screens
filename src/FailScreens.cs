8b976a1c4eb66e68404e6ef138a93cb99bbaf755
Andreas G <13819164+agaertner@users.noreply.github.com>
2024-04-17 15:27:13 +0200

    validate volume

--- src/FailScreens.cs
+++ src/FailScreens.cs
@@ -37,48 +37,53 @@ namespace Nekres.FailScreens {
 
         protected override void DefineSettings(SettingCollection settings) {
             var visualsCol = settings.AddSubCollection("visuals", true, () => "Defeated Screen");
-            FailScreen = visualsCol.DefineSetting("fail_screen", DefeatedService.FailScreens.DarkSouls, () => "Appearance", () => "Visual to display upon defeat.");
-            Random     = visualsCol.DefineSetting("random",      true,                                  () => "Randomize", () => "Ignores selection if set.");
-            var soundCol = settings.AddSubCollection("sound", true, () => "Sound Options");
-            Volume = soundCol.DefineSetting("volume", 50f,   () => "Volume", () => "Adjusts the audio volume.");
-            Muted        = soundCol.DefineSetting("mute",   false, () => "Mute",        () => "Mutes the audio.");
+            FailScreen = visualsCol.DefineSetting("fail_screen", DefeatedService.FailScreens.DarkSouls,
+                                                  () => "Appearance",
+                                                  () => "Visual to display upon defeat.");
+            Random     = visualsCol.DefineSetting("random",      true,
+                                                  () => "Randomize",
+                                                  () => "Ignores selection if set.");
+
+            var soundCol = settings.AddSubCollection("sound", true, () => "Sound Options"); 
+ Volume = soundCol.DefineSetting("volume", 0.05f,   
+                                            () => "Volume",
+                                            () => "Adjusts the audio volume.");
+            Muted = soundCol.DefineSetting("mute",   false,
+                                           () => "Mute",
+                                           () => "Mutes the audio.");
+
+            Volume.SetRange(0, 0.1f);
+            Volume.SetValidation(ValidateVolume);
+            if (!ValidateVolume(Volume.Value).Valid) {
+                Volume.Value = 0.05f;
+            }
         }
 
-        private void OnVolumeSettingChanged(object o, ValueChangedEventArgs<float> e) {
-            SoundVolume = Math.Min(GameService.GameIntegration.Audio.Volume, e.NewValue / 1000);
-        }
-
-        private void OnMutedSettingChanged(object o, ValueChangedEventArgs<bool> e) {
-            SoundVolume = e.NewValue ? 0f : Math.Min(GameService.GameIntegration.Audio.Volume, Volume.Value / 1000);
+        private SettingValidationResult ValidateVolume(float vol) {
+            return new SettingValidationResult(vol is >= 0f and <= 0.1f);
         }
 
         protected override void Initialize() {
-            State    = new StateService();
-            Defeated = new DefeatedService();
-
-            SoundVolume           =  Muted.Value ? 0f : Math.Min(GameService.GameIntegration.Audio.Volume, Volume.Value / 1000);
-            Volume.SettingChanged += OnVolumeSettingChanged;
-            Muted.SettingChanged  += OnMutedSettingChanged;
+            State                    =  new StateService();
+            Defeated                 =  new DefeatedService();
         }
-         
+
         protected override async Task LoadAsync() {
             await State.SetupLockFiles(StateService.State.Defeated);
         }
 
         protected override void Update(GameTime gameTime) {
             State?.Update();
+            SoundVolume = Muted.Value ? 0f : Math.Min(GameService.GameIntegration.Audio.Volume, Volume.Value);
         }
 
         protected override void OnModuleLoaded(EventArgs e) {
-
             // Base handler must be called
             base.OnModuleLoaded(e);
         }
 
         /// <inheritdoc />
         protected override void Unload() {
-            Muted.SettingChanged  -= OnMutedSettingChanged;
-            Volume.SettingChanged -= OnVolumeSettingChanged;
             Defeated?.Dispose();
             State?.Dispose();
             // All static members must be manually unset
